53082eb98fdc4acfeb21bc2ac5c392b81e492afc
bernardogabriel <tsunami6>
2018-11-19 11:22:59 -0200

    Teste

--- exercicio13.js
+++ exercicio13.js
@@ -1,6 +1,6 @@
-function foo() {
+function fooMais() {
 
-    var _numero1 = 1;
+    var _numero1 = 4.5;
     var _numero2 = 1;
     var _numeror = 0;
     _numeror = _numero1 + _numero2;
@@ -8,7 +8,7 @@ function foo() {
 };
 
 function fooMenos() {
-    var _numero1 = 1;
+    var _numero1 = 4.5;
     var _numero2 = 1;
     var _numeror = 0;
     _numeror = _numero1 - _numero2;
@@ -17,7 +17,7 @@ function fooMenos() {
 
 function fooDiv() {
     var _numero1 = 10;
-    var _numero2 = 2;
+    var _numero2 = 4.5;
     var _numeror = 0;
     _numeror = _numero1 / _numero2;
     alert(_numeror);
